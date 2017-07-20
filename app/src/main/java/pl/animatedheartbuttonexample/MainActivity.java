3f4483b56f7d8f96b5ae7076b031bdf12092aeb3
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2017-07-20 09:19:56 +0200

    Finished demo app

--- app/src/main/java/pl/animatedheartbuttonexample/MainActivity.java
+++ app/src/main/java/pl/animatedheartbuttonexample/MainActivity.java
@@ -3,6 +3,7 @@ package pl.animatedheartbuttonexample;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AnimatedHeartButton animatedHeartButton = findViewById(R.id.animated_heart_button);
+        animatedHeartButton.setChecked(true, false);
+        animatedHeartButton.setOnHeartButtonCheckedChangeListener(isChecked -> {
+            Toast.makeText(this, "isChecked = " + isChecked, Toast.LENGTH_SHORT).show();
+        });
     }
 }
